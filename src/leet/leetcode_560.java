b42556ba12cfb80fc01e0a9ca33f9012e01fcf56
budaniao88 <827348636@qq.com>
2024-01-19 17:01:10 +0800

    leetcode

---
+++ src/leet/leetcode_560.java
@@ -0,0 +1,44 @@
+package leet;
+
+import java.util.HashMap;
+
+/*
+* 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的子数组的个数 。
+
+子数组是数组中元素的连续非空序列。
+
+
+
+示例 1：
+
+输入：nums = [1,1,1], k = 2
+输出：2
+示例 2：
+
+输入：nums = [1,2,3], k = 3
+输出：2
+
+
+提示：
+
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107
+*
+* */
+public class leetcode_560 {
+    public int subarraySum(int[] nums, int k) {
+        if(nums == null || nums.length == 0) return 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        int count = 0;
+        int pre = 0;
+        for (int s = 0; s < nums.length; s++) {
+            pre += nums[s];
+            int dif = pre -k;
+            count += map.getOrDefault(dif,0);
+            map.put(pre,map.getOrDefault(pre,0)+1);
+        }
+        return  count;
+    }
+}
